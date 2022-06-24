b8f4ad8bd4a2f357342048db9ce02f07fed89da7
zastar23 <florin.porut@gmail.com>
2022-06-24 11:00:14 +0300

    Change to the crew page

--- assets/src/script/crew.js
+++ assets/src/script/crew.js
@@ -1,3 +1,37 @@
 import data from "../../../data.json" assert { type: "json" };
 
 console.log(data);
+const img = document.querySelector(".main--moon");
+const role = document.querySelector(".role");
+const headingName = document.querySelector(".name");
+const text = document.querySelector(".main--crew--info_text");
+
+const dotContainer = document.querySelector(".dot-container");
+
+// create dots
+const createDots = function () {
+	for (let i = 0; i <= 3; i++) {
+		const dot = document.querySelector(".main--crew-dots");
+
+		dotContainer.insertAdjacentHTML(
+			"beforeend",
+			`<div class="main--crew-dots" data-set = ${i + 1} ></div>`
+		);
+	}
+};
+
+createDots();
+
+// add event delegation
+
+dotContainer.addEventListener("click", function (e) {
+	if (e.target.classList.contains("main--crew-dots")) {
+		const dot = e.target.dataset;
+		if ((dot.dataset = "2")) {
+			role.innerHTML = data.crew[1].role;
+			headingName.innerHTML = data.crew[1].name;
+			img.src = data.crew[1].images.webp;
+			text.innerHTML = data.crew[1].bio;
+		}
+	}
+});
